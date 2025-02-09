6ce85ffc87440f19c6837400c80d3cd2010ef82b
sli9 <sil9gosy@mail.ru>
2025-02-10 00:28:04 +0300

    handle error, when nothing found

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,7 +9,11 @@ function App() {
 
   const debouncedValue = useDebounce(inputValue, 1000);
 
-  const { data: characters } = useGetCharactersQuery(debouncedValue.trim(), {
+  const {
+    data: characters,
+    isFetching,
+    error,
+  } = useGetCharactersQuery(debouncedValue.trim(), {
     skip: debouncedValue.trim().length < 4,
   });
 
@@ -18,8 +22,16 @@ function App() {
   };
 
   let charactersForRender = characters;
-  if (debouncedValue.trim().length < 4) {
+  if (debouncedValue.trim().length < 4 || !!error) {
     charactersForRender = undefined;
+  }
+
+  let errMsg = 'Something happened';
+  if (error) {
+    if ('data' in error) {
+      const errorData = error.data as { error: string };
+      errMsg = errorData.error;
+    }
   }
 
   return (
@@ -37,8 +49,10 @@ function App() {
             ? `Found characters: ${charactersForRender.length}`
             : ''}
         </span>
+        {error && <span style={{ color: '#ea2e34' }}>{errMsg}</span>}
       </div>
       <div>
+        {isFetching && <h1>Loading...</h1>}
         <CharactersList characters={charactersForRender} />
       </div>
     </>
--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -3,17 +3,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'charactersApi',
   baseQuery: async (args, api, extraOptions) => {
-    const result = await fetchBaseQuery({
+    return fetchBaseQuery({
       baseUrl: import.meta.env.VITE_BASE_URL,
     })(args, api, extraOptions);
-
-    // handleError(api, result);
-
-    return result;
   },
   endpoints: () => ({}),
-  keepUnusedDataFor: 5 * 60,
-  refetchOnFocus: true,
-  refetchOnReconnect: true,
   tagTypes: ['Characters'],
 });
